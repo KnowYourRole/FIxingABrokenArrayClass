d6485fbd57c40f009cd98a79e508e30ed5169431
Emil Dimitrov Antonov <emil.antonov2807@gmail.com>
2023-05-05 10:32:42 +0100

    Add files via upload

---
+++ FixedArray.cpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "FixedArray.h"
+
+FixedArray::FixedArray(const int arraySize) : BrokenArray(arraySize)
+{
+
+}
+FixedArray::~FixedArray()
+{
+	delete[] _array;
+}
+
+FixedArray::FixedArray(const FixedArray & other) : BrokenArray(other)
+{
+	int * newArray = new int[other.getSize()]{};
+
+	for (unsigned idx = 0; idx < other.getSize(); idx++)
+		newArray[idx] = other._array[idx];
+
+
+	if (_array != nullptr)
+	{
+		delete[] _array;
+	}
+	_array = newArray;
+	
+
+}
+FixedArray & FixedArray::operator=(const FixedArray & other)
+{
+	((BrokenArray *)this)->operator=(other);
+
+	int * newArray = new int[other.getSize()]{};
+
+	for (unsigned idx = 0; idx < other.getSize(); idx++)
+		newArray[idx] = other._array[idx];
+
+
+	if (_array != nullptr)
+	{
+		delete[] _array;
+	}
+	_array = newArray;
+
+	return *this;
+}
+
+void FixedArray::addValueToMemory(const int value)
+{
+	BrokenArray::addValueToMemory(value);
+}
+
+int FixedArray::getMemorySumValue() const
+{
+	return BrokenArray::getMemorySumValue();
+}
